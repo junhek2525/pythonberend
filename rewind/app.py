031f2af0195abb5300c18514454a16a0a19fa829
junhek2525 <junhek2525@gmail.com>
2024-05-24 15:04:32 +0900

    feat: follow

--- rewind/app.py
+++ rewind/app.py
@@ -27,6 +27,21 @@ def post():
         'post' : msg
     })
     return '성공', 200
-
+@app.route('/follow', methods=['POST'])
+def follow():
+    payload = request.json
+    userId = int(payload['id'])
+    userIdToFollow = int(payload['follow'])
+    
+    if userId not in app.users or userIdToFollow not in app.users:
+        return '사용자가 존제하지 않습니다', 400
+    
+    user = app.users[userId]
+    if user.get('follow'):
+        user['follow'].append(userIdToFollow)
+        user['follow'] = list(int(user['follow']))
+    else:
+        user['follow'] = [userIdToFollow]
+        return jsonify(user)
 if __name__ == '__main__':
     app.run()
