22a354057fb1e701bfa8ac0c3c3e138daf12f3ce
junhek2525 <junhek2525@gmail.com>
2024-06-21 15:21:50 +0900

    miniSNS sign-up

---
+++ mainSMS/app.py
@@ -0,0 +1,91 @@
+from flask import Flask, render_template , jsonify ,request
+
+app = Flask(__name__)
+app.users = {}
+app.posts = []
+app.idcut = 1
+
+@app.route('/')
+def root():
+    return """
+<h1>miniSNS</h1>
+<div>
+    <button>메인 페이지</button>
+    <button>회원 가입</button>
+    <button>포스팅</button>
+</div>
+"""
+
+@app.route('/main', methods=['GET'])
+def main():
+    return render_template('main.html', users = app.users)
+
+@app.route('/sign-up', methods=['POST'])
+def signUpPage():
+    return render_template('signup.html')
+@app.route('/post', methods=['POST'])
+def post():
+    payload = request.json
+    userID = int(payload['id'])
+    msg = payload['msg']
+
+    if userID not in app.users:
+        return '사용자가 존재하지 않습니다', 400
+    if len(msg) > 300:
+        return '300자를 초과했습니다', 400
+    
+    app.posts.append({
+        'userId' : userID,
+        'post' : msg
+    })
+    return '성공', 200
+
+@app.route('/follow', methods=['POST'])
+def follow():
+    payload = request.json
+    userId = int(payload['id'])
+    userIdToFollow = int(payload['follow'])
+    
+    if userId not in app.users or userIdToFollow not in app.users:
+        return '사용자가 존제하지 않습니다', 400
+    
+    user = app.users[userId]
+    if user.get('follow'):
+        user['follow'].append(userIdToFollow)
+        user['follow'] = list(int(user['follow']))
+    else:
+        user['follow'] = [userIdToFollow]
+        return jsonify(user)
+    
+@app.route('/unfollow', methods=['POST'])
+def unfollow():
+    payload = request.json
+    userId = int(payload['id'])
+    userIdToFollow = int(payload['unfollow'])
+    if userId not in app.users or userIdToFollow not in app.users:
+        return '사용자가 존제하지 않습니다', 400
+    
+    user = app.users[userId]
+    if user.get('follow'):
+        try:    user['follow'].remove(userIdToFollow)
+        except: pass
+    else:
+        user['follow'] = []
+        return jsonify(user)
+    
+@app.route('/timeline/<int:userId>', methods=['POST'])
+
+def timeline(userId):
+    if userId not in app.users:
+     return '사용자가 존제하지 않습니다', 400
+    if app.users[userId].get('follow'):
+     FollowList = set(app.users[userId]['follow'])
+    else:
+        FollowList = set()
+    FollowList.add(userId)
+    timeline = [msg for msg in app.posts if msg['userId'] in FollowList]
+
+
+
+if __name__ == '__main__':
+    app.run()
